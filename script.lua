8d53801f6eba2736cd1ec8e7f23e7e9435d2918d
x3sharkx2z <bevz91741@gmail.com>
2025-03-02 13:49:34 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,13 +1,1 @@
-if getgenv().Config['Auto Farm'] then
-    print('Auto Farm')
-end
-
--- ตรวจสอบ Style
-if getgenv().Config['Style']['Enabled'] == true and getgenv().Config['Style']["Isagi"] == true then
-    print('Isagi')
-end
-
--- ตรวจสอบ FpsBoost
-if getgenv().Config['FpsBoost'] then
-    print('FpsBoost')
-end
+game.Players.LocalPlayer:kick("กำลังทำ")
